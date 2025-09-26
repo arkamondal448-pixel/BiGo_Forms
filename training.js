0e8cc9db5d053dced60384b9486fe56d2909ef1b
arkamondal448-pixel <arkamondal448@gmail.com>
2025-09-26 11:29:48 +0530

    Update training.js

--- training.js
+++ training.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("trainingForm");
   const trainingTypeSelect = document.getElementById("trainingType");
   const previousJobGroup = document.getElementById("previousJobGroup");
-  const submitBtn = form.querySelector('button[type="submit"]'); // ✅ define submit button
+  
 
   const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwMC3no_08ZawTanj2ZmvSFgFAmhNIqv2fNcTdx1BtkJjS1ex7b5i9Uay4IgPlL4-Kdcg/exec"; // 🔹 replace
 
@@ -44,12 +44,9 @@ document.addEventListener("DOMContentLoaded", () => {
     form.reset();
     previousJobGroup.classList.add("hidden");
     
-  })
-  .finally(() => {
-            // Re-enable button after everything is done
-            submitBtn.disabled = false;
-            submitBtn.innerText = "Submit";
+  
         });
     });
   
+
 
