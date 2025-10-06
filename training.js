05a57be89c4e472da669c0eb07cb702fb086fd8f
Arka Mondal <arkamondal448@gmail.com>
2025-10-06 11:06:11 +0530

    Update training.js

--- training.js
+++ training.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
       visitorName: document.getElementById("visitorName").value,
       email: document.getElementById("email").value,
       phone: document.getElementById("phone").value,
-      altPhone: document.getElementById("altNumber").value,
+      altNumber: document.getElementById("altNumber").value,
       area: document.getElementById("area").value,
       reference: document.getElementById("reference").value,
       trainingType: document.getElementById("trainingType").value,
@@ -48,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
   
+
 
 
 
