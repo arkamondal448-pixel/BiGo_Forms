b497959ed8b53ef307097adb4eb92d854960879d
Arka Mondal <arkamondal448@gmail.com>
2025-10-06 11:02:52 +0530

    Update interview.js

--- interview.js
+++ interview.js
@@ -51,7 +51,7 @@ form.addEventListener("submit", (e) => {
       cv_filename: file.name
     };
 
-    fetch('https://script.google.com/macros/s/AKfycbwMC3no_08ZawTanj2ZmvSFgFAmhNIqv2fNcTdx1BtkJjS1ex7b5i9Uay4IgPlL4-Kdcg/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzGvzNnfwcHg_K1lccWt_hqPBLhFs4WU7Ir6cZok3yc68Gz5IQfrC0nGKvIfbqUFMCfUg/exec', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: { 'Content-Type': 'text/plain' }
@@ -69,3 +69,4 @@ form.addEventListener("submit", (e) => {
 
   reader.readAsDataURL(file);
 });
+
